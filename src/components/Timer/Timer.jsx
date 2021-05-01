4f9100b2b585494900287a1fbf347230f5430789
Евгений Чип <evgesha.chip.98@mail.ru>
2021-05-02 00:20:48 +0300

    верстка Timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Timer.css';
 
 export const Timer = () =>{
+    const [task, setTask] = useState('')
+
+    const handleTask = (event) =>{
+        
+        setTask(event.target.value)
+        console.log(task)
+    }
     return(
         <div className='Timer'>
-            <h3>25:00</h3>
-            <button>Старт</button>
-            <button>Стоп</button>
+            <div className='timerLogo'>
+                 <h3 >25:00</h3>
+            </div>
+            <div style={{display:'flex'}}>
+                <button className='btn'>Старт</button>
+                <button className='btn'>Стоп</button>
+            </div>
             <form>
-            <input type='text' />
-            <button>Сохранить</button>
+            <select className='timerSelect'></select>
+            <input 
+                onChange={handleTask}
+                className='timerInput'
+                type='text'
+                placeholder='что сделал'
+             />
+            <button className='btnSave'>Сохранить</button>
             </form>
         </div>
     )
